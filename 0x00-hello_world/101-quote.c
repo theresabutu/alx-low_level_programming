9626cf8b4ee5352ea8cbc7b075a99bb83db06beb
theresabutu <theresa.abutu@gmail.com>
2023-04-24 00:24:10 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+  *main - This is a c program that prints a file to the standard error
+  *Return: Always 1 {success}
+  */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
