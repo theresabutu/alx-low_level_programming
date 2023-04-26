c6a117c53f58517689c46d1d0b4d717021dd0f3e
theresabutu <theresa.abutu@gmail.com>
2023-04-25 20:02:23 -0700

    2-largest_number.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,29 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+
 
 /**
  * main - Determines if a number is positive, negative or zero.
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
+	if (i > 0)
+		printf("%d is positive\n", i);
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+	else if (i<0)
+		printf("%d is negative\n", i);
+
+	else 
+		printf("%d is zero\n", i);
 }
